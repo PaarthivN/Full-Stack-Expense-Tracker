d1c509d3fb8eb5cd21ef1efa0c13e978e1afe71f
PaarthivN <Pneelapu17@gmail.com>
2024-08-14 13:17:07 +0530

    Update midforauthentication.js

--- Backend/midforauthentication.js
+++ Backend/midforauthentication.js
@@ -1,1 +1,18 @@
+// middleware/auth.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
+const authenticate = async (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(403).send('Token required');
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id);
+        next();
+    } catch (error) {
+        res.status(401).send('Invalid Token');
+    }
+};
+
+module.exports = { authenticate };
