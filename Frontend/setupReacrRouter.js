6cd6119d014449eb42a29ef4df20a8833d8cc030
PaarthivN <Pneelapu17@gmail.com>
2024-08-14 13:21:54 +0530

    Update setupReacrRouter.js

--- Frontend/setupReacrRouter.js
+++ Frontend/setupReacrRouter.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import PieChart from './components/PieChart';
 
+function App() {
+    return (
+        <Router>
+            <div>
+                <Switch>
+                    <Route path="/signup" component={Signup} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/expenses/new" component={ExpenseForm} />
+                    <Route path="/expenses" component={ExpenseList} />
+                    <Route path="/chart" component={PieChart} />
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
