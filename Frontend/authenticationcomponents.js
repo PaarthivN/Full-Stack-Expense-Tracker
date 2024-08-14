cbf5fc4cecb96a7f5e8a328c5c724c2bfb34a432
PaarthivN <Pneelapu17@gmail.com>
2024-08-14 13:25:01 +0530

    Update authenticationcomponents.js

--- Frontend/authenticationcomponents.js
+++ Frontend/authenticationcomponents.js
@@ -1,1 +1,23 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 
+const Signup = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:5000/api/auth/signup', { username, password });
+        alert('User Created');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <button type="submit">Sign Up</button>
+        </form>
+    );
+};
+
+export default Signup;
